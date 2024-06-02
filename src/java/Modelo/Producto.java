4fb47d37d4e631db09e6afd921e022ca52268132
AJsagitario <u22104232@utp.edu.pe>
2024-06-02 13:23:27 -0500

    Angel Lopez

--- src/java/Modelo/Producto.java
+++ src/java/Modelo/Producto.java
@@ -8,62 +8,19 @@ public class Producto {
     private int stock;
     private String imagen;
     private int categoria;
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public String getDescripcion() {return descripcion;}
+    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
+    public double getPrecio() {return precio;}
+    public void setPrecio(double precio) { this.precio = precio;}
+    public int getStock() {return stock;}
+    public void setStock(int stock) {this.stock = stock;}
+    public String getImagen() {return imagen;}
+    public void setImagen(String imagen) {this.imagen = imagen;}
+    public int getCategoria() {return categoria;}
+    public void setCategoria(int categoria) {this.categoria = categoria;}}
 
-    public Producto() {}
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public int getategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(int categoria) {
-        this.categoria = categoria;
-    }
-}
+   
